78cd63789ad3475b3a045285e6776c41f1cc6e3b
chongzixin <3539074+chongzixin@users.noreply.github.com>
2020-10-20 21:30:31 +0800

    show timestamp on a flatlist. still some errors left

--- App.js
+++ App.js
@@ -20,20 +20,32 @@ const App = () => {
   const [deviceName, setDeviceName] = useState('');
   NativeModules.Device.getDeviceName((err, name) => setDeviceName(name));
 
+  // TODO: read flatlist data from a file and use it to init the timestampList
+  const [timestampList, setTimestampList] = useState([]);
+
   useEffect(() => {
+    // TODO: this seems to be causing memory leaks
     const eventEmitter = new NativeEventEmitter(NativeModules.AlarmModule);
     if(!this.eventListener) {
       this.eventListener = eventEmitter.addListener('ALARM_EVENT', (event) => {
-        console.log(event); 
-
         // alarm has occurred. add the event to a state
+        console.log("current event is: " + event); 
+        setTimestampList(currentList => [event, ...currentList]);
+        // TODO: write to a file
       });
     }
   });
 
   return (
     <View style={styles.screen}>
-      <Text>We are testing Alarm here from {deviceName}</Text>
+      <View style={styles.listContainer}>
+        <FlatList 
+          contentContainerStyle={styles.list}
+          data={timestampList}
+          renderItem={itemData => (<Text>{itemData.item}</Text>)}
+          keyExtractor={item => item}
+        />
+      </View>
     </View>
   );
 };
@@ -41,9 +53,10 @@ const App = () => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+  },
+  listContainer: {
+    paddingHorizontal: 20,
+  },
 });
 
 export default App;
